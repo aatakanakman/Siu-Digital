9aa4c364df9ae775779574019f66468243fe62a2
aatakanakman <aatakanakman@gmail.com>
2021-11-09 13:41:34 +0300

    DONE

--- src/controllers/UserController/user.controller.ts
+++ src/controllers/UserController/user.controller.ts
@@ -20,41 +20,59 @@ export default class UserController {
   //Create User Endpoint
   @Post()
   async createUser(@Res() response, @Body() user: User) {
-    const newUser = await this.UserService.create(user);
-    return response.status(HttpStatus.CREATED).json({
-      newUser,
-    });
+    try {
+      const newUser = await this.UserService.create(user);
+      return response.status(HttpStatus.CREATED).json({
+        newUser,
+      });
+    } catch (error) {
+      return response.status(HttpStatus.BAD_REQUEST).json({
+        error,
+      });
+    }
   }
 
   //Update User Endpoint
   @Put('/:id')
   async update(@Res() response, @Param('id') id, @Body() user: User) {
-    const updatedUser = await this.UserService.update(id, user);
-    return response.status(HttpStatus.OK).json({
-      updatedUser,
-    });
+    try {
+      const updatedUser = await this.UserService.update(id, user);
+      return response.status(HttpStatus.OK).json({
+        updatedUser,
+      });
+    } catch (error) {
+      return response.status(HttpStatus.BAD_REQUEST).json({
+        error,
+      });
+    }
   }
 
   //Get All User Endpoint
   @Get()
   async fetchAll(@Res() response) {
-    const users = await this.UserService.readAll();
-    return response.status(HttpStatus.OK).json({
-      users,
-    });
+    try {
+      const users = await this.UserService.readAll();
+      return response.status(HttpStatus.OK).json({
+        users,
+      });
+    } catch (error) {
+      return response.status(HttpStatus.BAD_REQUEST).json({
+        error,
+      });
+    }
   }
 
   // Update all user fields
   //It updates a new field from the Body for all collection members.
   @Put('/add/new_field')
-  async addField(@Res() res, @Body() key: Record<string, unknown>) {
+  async addField(@Res() response, @Body() key: Record<string, unknown>) {
     try {
       await this.UserService.addNewField(key);
-      return res
+      return response
         .status(HttpStatus.OK)
         .json({ message: 'Add new field success', key });
     } catch (err) {
-      return res.status(HttpStatus.BAD_REQUEST).json({
+      return response.status(HttpStatus.BAD_REQUEST).json({
         err,
       });
     }
@@ -63,9 +81,15 @@ export default class UserController {
   //Delete User Endpoint
   @Delete('/:id')
   async delete(@Res() response, @Param('id') id) {
-    const deletedUser = await this.UserService.delete(id);
-    return response.status(HttpStatus.OK).json({
-      deletedUser,
-    });
+    try {
+      const deletedUser = await this.UserService.delete(id);
+      return response.status(HttpStatus.OK).json({
+        deletedUser,
+      });
+    } catch (err) {
+      return response.status(HttpStatus.BAD_REQUEST).json({
+        err,
+      });
+    }
   }
 }
